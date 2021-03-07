c46938a48674d1627a380a0d0bb675ee419f3673
imoishi <cse.oishi@gmail.com>
2021-03-07 19:19:55 +0600

    template class & setup

---
+++ templates/inc/footer.php
@@ -0,0 +1,8 @@
+               <footer class="footer">
+        <p>&copy; 2021 JobLister, Inc.</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+       </body>
+</html>
---
+++ templates/inc/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JobLister</title>
+	<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.css">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	
+<div class="container">
+			<nav class="navbar navbar-expand-sm navbar-white fixed-top bg-white">
+  <a class="navbar-brand text-muted mr-auto" href="#">Joblister</a>
+
+    <ul class="navbar-nav ">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="create.php">Create Listing</a>
+      </li>
+    </ul>
+
+</nav>
+<hr>
+      
